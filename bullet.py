8914f84b49f373c84b756f0f231f05cea9176874
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-04 11:45:39 +0200

    Created class bullet for the pirate

---
+++ bullet.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+        
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
