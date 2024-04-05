c306e2c0f95c1ec4cd2bf989aed865a564ba1af7
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-05 15:11:34 +0200

    playable version of tuto

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame_gui
+from pirate import Pirate
 
 def Tutorial():
     
@@ -14,6 +15,41 @@ def Tutorial():
         manager=MANAGER,
         object_id="button",
         tool_tip_text="Start the game")
+    label_tutorial = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 20), (500, 400)),
+        text="Tutorial",
+        manager=MANAGER,
+        object_id="label")
+    label_tutorial_text = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 60), (500, 400)),
+        text="This is a tutorial on how to play the game.",
+        manager=MANAGER,
+        object_id="label")
+    label_tutorial_text_2 = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 80), (500, 400)),
+        text="Press the start button to start the game.",
+        manager=MANAGER,
+        object_id="label")
+    label_tutorial_text_3 = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 120), (500, 400)),
+        text="Use the keys 'q' and 'd' or '←'and '→' to move the pirate",
+        manager=MANAGER,
+        object_id="label")
+    label_tutorial_text_4 = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 100), (500, 400)),
+        text="Press the space key to shoot the pirate's cannon.",
+        manager=MANAGER,
+        object_id="label")
+    label_tutorial_text_5 = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 140), (500, 400)),
+        text="Press the escape key to quit the game.",
+        manager=MANAGER,
+        object_id="label")
+    all_sprites = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
+    pirate = Pirate(all_sprites, bullets, 540, 480)
+    all_sprites.add(pirate)
+    
     running = True
     while running:
         for event in pygame.event.get():
@@ -22,15 +58,18 @@ def Tutorial():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                if event.key == pygame.K_SPACE:
+                    pirate.shoot()
             if event.type == pygame.USEREVENT:
                 if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                     if event.ui_element == button_start:
                         print("Start button clicked")
+                        running = False
             MANAGER.process_events(event)
         MANAGER.update(clock.tick(60))
         screen.fill((255, 255, 255))
+        all_sprites.update()
+        all_sprites.draw(screen)
         MANAGER.draw_ui(screen)
         pygame.display.flip()
     pygame.quit()
-    
-Tutorial()
