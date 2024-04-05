7fcb7a91d8b58dced9429588b31df65ffef72098
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-05 14:11:48 +0200

    Play button added to the window

--- tutorial.py
+++ tutorial.py
@@ -1,13 +1,19 @@
 import pygame
+import pygame_gui
 
 def Tutorial():
-    statisticsBar = pygame.Rect(0, 0, 60, 640) 
-
+    
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((540, 640), pygame.NOFRAME)
     screen.fill((255, 255, 255))
-
-    pygame.draw.rect(screen, (0, 0, 0), statisticsBar)
+    MANAGER = pygame_gui.UIManager((540, 640), "theme.json")
+    button_start = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((220, 560), (100, 40)),
+        text="START",
+        manager=MANAGER,
+        object_id="button",
+        tool_tip_text="Start the game")
     running = True
     while running:
         for event in pygame.event.get():
@@ -16,5 +22,15 @@ def Tutorial():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == button_start:
+                        print("Start button clicked")
+            MANAGER.process_events(event)
+        MANAGER.update(clock.tick(60))
+        screen.fill((255, 255, 255))
+        MANAGER.draw_ui(screen)
         pygame.display.flip()
     pygame.quit()
+    
+Tutorial()
