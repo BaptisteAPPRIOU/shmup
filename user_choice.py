3f1343aa64606927f92bd4dfc714e66a4f3cb3db
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-05 11:34:29 +0200

    Added play button working

--- user_choice.py
+++ user_choice.py
@@ -8,12 +8,11 @@ def UserChoice():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((540, 640), pygame.NOFRAME)
     screen.fill((255, 255, 255))
-    statisticsBar = pygame.Rect(0, 0, 60, 640) 
-    pygame.draw.rect(screen, (0, 0, 0), statisticsBar)
     MANAGER = pygame_gui.UIManager((540, 640))
+
     Button = pygame_gui.elements.UIButton(
         relative_rect=pygame.Rect((125, 70), (290, 100)),
-        text="Button",
+        text="PLAY",
         manager=MANAGER,
         object_id="button",
         tool_tip_text="Play"
@@ -29,7 +28,10 @@ def UserChoice():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+            MANAGER.process_events(event)
         
+        MANAGER.update(clock.tick(60))
+        screen.fill((255, 255, 255))
         MANAGER.draw_ui(screen)
         pygame.display.flip()
     pygame.quit()
