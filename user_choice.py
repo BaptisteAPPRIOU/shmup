b8ec051d89820d8bf67ca3bcf3c8b585233b7e8c
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-05 11:06:46 +0200

    Error on button display

--- user_choice.py
+++ user_choice.py
@@ -1,13 +1,26 @@
 import pygame
+import pygame_gui
 
 def UserChoice():
-    statisticsBar = pygame.Rect(0, 0, 60, 640) 
 
     pygame.init()
+    
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((540, 640), pygame.NOFRAME)
     screen.fill((255, 255, 255))
-
+    statisticsBar = pygame.Rect(0, 0, 60, 640) 
     pygame.draw.rect(screen, (0, 0, 0), statisticsBar)
+    MANAGER = pygame_gui.UIManager((540, 640))
+    Button = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((125, 70), (290, 100)),
+        text="Button",
+        manager=MANAGER,
+        object_id="button",
+        tool_tip_text="Play"
+    )
+
+    #pygame_gui.UIManager.update(RefreshRate)
+    #pygame_gui.UIManager.draw_ui((540, 640))
     running = True
     while running:
         for event in pygame.event.get():
@@ -16,5 +29,9 @@ def UserChoice():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+        
+        MANAGER.draw_ui(screen)
         pygame.display.flip()
     pygame.quit()
+    
+UserChoice()
