7674042f94e07aa84c73d35901666bbbd437f109
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-06 19:20:32 +0200

    Added image of cannon ball and logic of enemy cannon ball

---
+++ cannon_ball_enemy.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+import os
+
+class CannonBallEnemy(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join("images", "cannon_ball_enemies.png")).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.mask = pygame.mask.from_surface(self.image)
+        self.speed = 5
+
+    def update(self):
+        self.move()
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def move(self):
+        self.rect.y += self.speed
