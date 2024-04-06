fd9c456ace200202dcda78a13301b788d9471de8
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-06 17:19:36 +0200

    Added comments to the file

--- enemy_wave.py
+++ enemy_wave.py
@@ -3,11 +3,9 @@ import random
 from enemy import Enemy
 import sys
 import os
-from lifeboat_test import Lifeboat
-from sloop_test import Sloop
 
 class Wave:
-    def __init__(self, screen, vessels, enemy_types, counts, pause_duration):
+    def __init__(self, screen, vessels, enemy_types, counts, pause_duration):                       
         self.screen = screen
         self.vessels = vessels
         self.enemy_types = enemy_types
@@ -16,25 +14,29 @@ class Wave:
         self.timer = 0
         self.current_wave = 0
 
-    def update(self, dt):
-        if self.current_wave < len(self.enemy_types):
-            enemy_type = self.enemy_types[self.current_wave]
-            count = self.counts[self.current_wave]
-            if count > 0:
-                self.timer += dt
-                if self.timer >= 3:  # Delay in seconds between enemy spawns
-                    self.timer = 0
-                    x = random.randint(80, 520 - enemy_type.width)
-                    y = random.randint(-1500, -300)
-                    while self.check_overlap(x, y, enemy_type):
-                        x = random.randint(80, 520 - enemy_type.width)
-                        y = random.randint(-1500, -300)
-                    enemy = enemy_type(self.screen, self.vessels, speed=enemy_type.speed, x=x, y=y)
-                    self.vessels.add(enemy)
-                    self.counts[self.current_wave] -= 1
+    def spawn_waves(self, dt):                                                      # Spawn waves of enemies
+        if self.current_wave < len(self.enemy_types):                               # Check if there are more waves to spawn
+            enemy_type = self.enemy_types[self.current_wave]                        # Get the enemy type for the current wave       
+            count = self.counts[self.current_wave]                                  # Get the number of enemies to spawn for the current wave
+
+            if count > 0:                                                           # Check if there are enemies left to spawn
+                self.timer += dt                                                    # Increment timer during enemy spawn 
+                               
+                if self.timer >= 3:                                                 # Delay in seconds between enemy spawns
+                    self.timer = 0                                                  # Reset timer             
+                    x = random.randint(80, 520 - enemy_type.width)                  # Random x-coordinate for enemy spawn       
+                    y = random.randint(-1500, -300)                                 # Random y-coordinate for enemy spawn
+                    
+                    while self.check_overlap(x, y, enemy_type):                     # Check if the enemy overlaps with other enemies
+                        x = random.randint(80, 520 - enemy_type.width)              # Random x-coordinate for enemy spawn
+                        y = random.randint(-1500, -300)                             # Random y-coordinate for enemy spawn
+                    enemy = enemy_type(self.screen, self.vessels, speed=enemy_type.speed, x=x, y=y)             # Create an enemy object
+                    
+                    self.vessels.add(enemy)                                         # Add the enemy to the vessels group
+                    self.counts[self.current_wave] -= 1                             # Decrement the number of enemies left to spawn
             else:
-                self.timer += dt  # Increment timer during pause
-                if self.timer >= self.pause_duration:  # Delay in seconds between waves
+                self.timer += dt                                                    # Increment timer during pause
+                if self.timer >= self.pause_duration:                               # Delay in seconds between waves
                     self.timer = 0
                     self.current_wave += 1
 
