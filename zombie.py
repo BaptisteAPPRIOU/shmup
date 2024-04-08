e071eb9bc9692998f7fcf0ee3d7fe72835221a56
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-08 14:32:50 +0200

    Clean commit with commentary

--- zombie.py
+++ zombie.py
@@ -1,13 +1,12 @@
 import pygame
 import os
-from poison_gas import PoisonGas
 import random
+from poison_gas import PoisonGas
 
 class Zombie(pygame.sprite.Sprite):
     def __init__(self, x, y, gas_sprites):
         super().__init__()
-        # Load zombie images
-        self.images = [pygame.image.load(f"images/zombie1_{i}.png").convert_alpha() for i in range(1, 5)]
+        self.images = [pygame.image.load(f"images/zombie1_{i}.png").convert_alpha() for i in range(1, 5)]   # Load zombie images
         self.index = 0
         self.image = self.images[self.index]
         self.rect = self.image.get_rect(topleft=(x, y))
@@ -18,27 +17,25 @@ class Zombie(pygame.sprite.Sprite):
         self.last_shot = pygame.time.get_ticks()
         self.shoot_delay = 2000  # 2 seconds interval for shooting gas
 
-    def update(self, player_x, player_y):
+    def update(self, pirate_x, pirate_y):                               # Update the zombie and attack
         self.rect.y += self.speed
         self.animation_counter += 1
         if self.animation_counter >= self.animation_delay:
             self.animation_counter = 0
             self.index = (self.index + 1) % len(self.images)
             self.image = self.images[self.index]
-
         if self.rect.bottom > 900:
             self.rect.y = 0
-
         self.attack()
 
-    def attack(self):
+    def attack(self):                                                   # Attack the player
         now = pygame.time.get_ticks()
         if now - self.last_shot > self.shoot_delay:
-            self.shoot_delay = random.randint(1000, 3000)  # Randomize the shoot delay
+            self.shoot_delay = random.randint(1000, 3000)               # Randomize the shoot delay
             self.last_shot = now
             self.shoot_poison_gas()
 
-    def shoot_poison_gas(self):
+    def shoot_poison_gas(self):                                        # Shoot poison gas                               
         poison_gas = PoisonGas(self.rect.centerx, self.rect.bottom)
         poison_gas.change_y = 5
         self.gas_sprites.add(poison_gas)
--- zombie_2.py
+++ zombie_2.py
@@ -4,12 +4,10 @@ import math
 import random
 from poison_gas import PoisonGas
 
-
 class Zombie_2(pygame.sprite.Sprite):
     def __init__(self, x, y, gas_sprites):
         super().__init__()
-        # Load zombie images
-        self.images = [pygame.image.load(f"images/zombie1_{i}.png").convert_alpha() for i in range(1, 5)]
+        self.images = [pygame.image.load(f"images/zombie1_{i}.png").convert_alpha() for i in range(1, 5)]   # Load zombie images
         self.index = 0
         self.image = self.images[self.index]
         self.rect = self.image.get_rect(topleft=(x, y))
@@ -20,40 +18,29 @@ class Zombie_2(pygame.sprite.Sprite):
         self.last_shot = pygame.time.get_ticks()
         self.shoot_delay = 2000  # 2 seconds interval for shooting gas
 
-    def update(self, piratex, piratey):
+    def update(self, pirate_x, pirate_y):                                 # Update the zombie and attack
         self.rect.y += self.speed
         self.animation_counter += 1
         if self.animation_counter >= self.animation_delay:
             self.animation_counter = 0
             self.index = (self.index + 1) % len(self.images)
             self.image = self.images[self.index]
-
         if self.rect.bottom > 900:
             self.rect.y = 0
+        self.attack(pirate_x, pirate_y)
 
-        self.attack(piratex, piratey)
-
-    def attack(self, piratex, piratey):
+    def attack(self, pirate_x, pirate_y):                                 # Attack the player
         now = pygame.time.get_ticks()
         if now - self.last_shot > self.shoot_delay:
-            self.shoot_delay = random.randint(1000, 3000)  # Randomize the shoot delay
+            self.shoot_delay = random.randint(1000, 3000)               # Randomize the shoot delay
             self.last_shot = now
-            # self.shoot_poison_gas()
-            self.aim_at_pirate(piratex, piratey)
+            self.shoot_poison_gas(pirate_x, pirate_y)
 
-    def shoot_poison_gas(self):
-        poison_gas = PoisonGas(self.rect.centerx, self.rect.bottom)
-        self.gas_sprites.add(poison_gas)
-
-    def aim_at_pirate(self, pirate_x, pirate_y):
+    def shoot_poison_gas(self, pirate_x, pirate_y):                     # Shoot poison gas
         target_x = pirate_x - self.rect.centerx
         target_y = pirate_y - self.rect.centery
         angle = math.atan2(target_y, target_x)
         poison_gas = PoisonGas(self.rect.centerx, self.rect.bottom)
-        rotated_image = pygame.transform.rotate(self.images[self.index], math.degrees(angle))
-        poison_gas.image = rotated_image
         poison_gas.change_x = math.cos(angle) * 5
         poison_gas.change_y = math.sin(angle) * 5
-        
-        # poison_gas = pygame.transform.rotate(self.images[self.index], math.degrees(angle))
         self.gas_sprites.add(poison_gas)
