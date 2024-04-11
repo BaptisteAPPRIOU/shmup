0437c1951747c34ed6922c41b2c6078fd72ebe95
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-11 16:42:46 +0200

    Pirate stats can now be modified

--- pirate.py
+++ pirate.py
@@ -2,7 +2,7 @@ import pygame
 from bullet import Bullet
 
 class Pirate(pygame.sprite.Sprite):
-    def __init__(self, all_sprites, bullets,vessels,zombies,cannon_ball_enemy, WIDTH, HEIGHT):
+    def __init__(self, all_sprites, bullets,vessels,zombies,cannon_ball_enemy,damage, WIDTH, HEIGHT):
         pygame.sprite.Sprite.__init__(self)
         pirate_image = pygame.image.load("images/pirate.png").convert_alpha()
         pirate_image = pygame.transform.scale(pirate_image, (64, 64))
@@ -12,21 +12,22 @@ class Pirate(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT
         self.speedx = 0
-        self.health = 100
+        self.health = 1000
         self.all_sprites = all_sprites
         self.bullets = bullets
         self.WIDTH = WIDTH
+        self.damage = damage
         self.vessels = vessels
         self.zombies = zombies
         self.cannon_ball_enemy = cannon_ball_enemy
 
-    def update(self):
+    def update(self,i):
         self.speedx = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_q] or keystate[pygame.K_LEFT]:                     # Changed to allow the pirate to move with the keys "q" and "d"
-            self.speedx = -8
+            self.speedx = -(8*i)
         if keystate[pygame.K_d] or keystate[pygame.K_RIGHT]:                    # Changed to allow the pirate to move with the keys "q" and "d"     
-            self.speedx = 8
+            self.speedx = 8*i
         self.rect.x += self.speedx
 
         if self.rect.right > self.WIDTH:                                        # Adjusted to allow the pirate to move up to x = 540
@@ -35,6 +36,6 @@ class Pirate(pygame.sprite.Sprite):
             self.rect.left = 100
 
     def shoot(self):
-        bullet = Bullet(self.rect.centerx, self.rect.top, self.vessels, self.zombies, self.cannon_ball_enemy)
+        bullet = Bullet(self.rect.centerx, self.rect.top, self.vessels, self.zombies, self.cannon_ball_enemy, self.damage)
         self.all_sprites.add(bullet)
         self.bullets.add(bullet)
