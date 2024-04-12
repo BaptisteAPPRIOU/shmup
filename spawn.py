fe9723b867697737f876aa9048c6afaac9c47f8b
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-12 10:11:16 +0200

    Blood animation added

--- spawn.py
+++ spawn.py
@@ -1,10 +1,10 @@
 from zombie import Zombie
 from zombie2 import Zombie_2
 
-def SpawnZombie(co_x, co_y, zombies, zombie_type, poison_gas):
-    if zombie_type == 1:  # Spawn zombie type 1
-        zombie = Zombie(co_x, co_y, zombies, poison_gas)
+def SpawnZombie(co_x, co_y, zombies, zombie_type, poison_gas, blood):
+    if zombie_type == 1:                                                                        # Spawn zombie type 1
+        zombie = Zombie(co_x, co_y, zombies, poison_gas, blood)
         zombies.add(zombie)
-    elif zombie_type == 2:  # Spawn zombie type 2
-        zombie = Zombie_2(co_x, co_y, zombies, poison_gas)
+    elif zombie_type == 2:                                                                      # Spawn zombie type 2
+        zombie = Zombie_2(co_x, co_y, zombies, poison_gas, blood)
         zombies.add(zombie)
--- zombie2.py
+++ zombie2.py
@@ -1,11 +1,14 @@
 import pygame
+import os
 import math
 import random
 from poison_gas import PoisonGas
+from blood import Blood
 
 class Zombie_2(pygame.sprite.Sprite):
-    def __init__(self, x, y, zombies, poison_gas):
+    def __init__(self, x, y, zombies, poison_gas,blood):
         super().__init__()
+        pygame.mixer.init()
         self.images = [pygame.image.load(f"images/zombie2_{i}.png").convert_alpha() for i in range(1, 5)]   # Load zombie images
         self.index = 0
         self.image = self.images[self.index]
@@ -21,8 +24,10 @@ class Zombie_2(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)                                                                    # Create a mask from the image to use for collision detection
         self.hit_points = 350
         self.mask = pygame.mask.from_surface(self.image)                                                                    # Create a mask from the image to use for collision detection
+        self.blood = blood
+        self.blood_sound = pygame.mixer.Sound("sounds/blood.mp3")
 
-    def update(self, pirate_x, pirate_y):                                 # Update the zombie and attack
+    def update(self, pirate_x, pirate_y, blood):                                 # Update the zombie and attack
         self.rect.y += self.speed
         self.animation_counter += 1
         if self.animation_counter >= self.animation_delay:
@@ -57,4 +62,11 @@ class Zombie_2(pygame.sprite.Sprite):
     def update_hit_points(self, damage):                                # Update the hit points of the zombie
         self.hit_points -= damage
         if self.hit_points <= 0:
-            self.kill()
+            self.die()
+
+    def die(self):                                                                                                      # Method to destroy the zombie
+        blood = Blood(self.rect.centerx, self.rect.centery)
+        self.blood.add(blood)
+        self.hit_points = 0
+        self.kill()
+        self.blood_sound.play()
